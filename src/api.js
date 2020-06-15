61257050e83e316225b2426dd7be1fa9637aaa25
Henrique Silva <60949741+heryssrique@users.noreply.github.com>
2020-06-15 10:06:31 -0300

    1.8
    agenda

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://heliotarefasbackend.herokuapp.com'
+    baseURL: 'https://helioagendabackend.herokuapp.com'
 });
 
 export default api;
